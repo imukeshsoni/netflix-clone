ef89f0453351a7b29535c55b167a5c9d359f31c4
Mukesh Soni <msony552@gmail.com>
2023-07-31 18:59:01 +0530

    added row component

---
+++ src/row/Row.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+import axios from '../network/axios';
+
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className='row'>
+      <h2>{title}</h2>
+
+      <div className='row__posters'>
+        {movies.map((movie) => {
+          return (
+            <img
+              className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+              key={movie.id}
+              src={`${process.env.REACT_APP_BASE_IMAGE_URL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
