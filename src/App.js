80e13d5d0df6fc557085b0d0094e2b0bc4d500f2
Mukesh Soni <msony552@gmail.com>
2023-08-05 13:07:01 +0530

    added redux store for user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/login/LoginScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/profile/ProfileScreen';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // Logged out
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <div className='app'>
       <Router>
@@ -14,6 +38,7 @@ function App() {
         ) : (
           <Routes>
             <Route path='/' Component={HomeScreen}></Route>
+            <Route path='/profile' Component={ProfileScreen}></Route>
           </Routes>
         )}
       </Router>
